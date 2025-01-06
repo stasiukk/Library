600a1a5584458eb446c79166821181569e4334f3
sarahstanazai <sarah.nabi1x@gmail.com>
2025-01-06 15:41:51 -0500

    updated Sarah's project

--- src/test/java/com/libraryApp/step_definitions/ApiStepDefs.java
+++ src/test/java/com/libraryApp/step_definitions/ApiStepDefs.java
@@ -23,6 +23,28 @@ import java.util.Map;
 
 public class ApiStepDefs {
 
+    @Given("I logged Library api as a {string} sn")
+    public void i_logged_library_api_as_a_sn(String string) {
 
+    }
+    @Given("Accept header is {string} sn")
+    public void accept_header_is_sn(String string) {
 
+    }
+    @When("I send GET request to “\\/get_all_users” endpoint sn")
+    public void i_send_get_request_to_get_all_users_endpoint_sn() {
+
+    }
+    @Then("status code should be {int} sn")
+    public void status_code_should_be_sn(Integer int1) {
+
+    }
+    @Then("Response Content type is {string} sn")
+    public void response_content_type_is_sn(String string) {
+
+    }
+    @Then("{string} field should not be null sn")
+    public void field_should_not_be_null_sn(String string) {
+
+    }
 }
