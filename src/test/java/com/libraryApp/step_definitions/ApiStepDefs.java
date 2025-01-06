c792bb5feb054b1eea6f5322cbd566c76855c4c6
stasiukk <andriistasiuk26@gmail.com>
2025-01-06 15:12:16 -0500

    added pages

---
+++ src/test/java/com/libraryApp/step_definitions/ApiStepDefs.java
@@ -0,0 +1,28 @@
+package com.libraryApp.step_definitions;
+
+// Importing necessary classes and libraries for the test
+
+import com.libraryApp.pages.DashBoardPage;
+import com.libraryApp.pages.LoginPage;
+import com.libraryApp.utilities.BrowserUtil;
+import com.libraryApp.utilities.DB_Util;
+import com.libraryApp.utilities.LibraryUtils;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Assert;
+
+import java.util.List;
+import java.util.Map;
+
+public class ApiStepDefs {
+
+
+
+}
