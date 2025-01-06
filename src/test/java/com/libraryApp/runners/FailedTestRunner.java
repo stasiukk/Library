bc9fcc9f7abd86faa07b93a07c17a6dc5e369deb
stasiukk <andriistasiuk26@gmail.com>
2025-01-06 15:13:24 -0500

    added runner class

---
+++ src/test/java/com/libraryApp/runners/FailedTestRunner.java
@@ -0,0 +1,15 @@
+package com.libraryApp.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "@target/rerun.txt",
+        glue = "com/libraryApp/step_definitions")
+
+public class FailedTestRunner {
+
+}
+
